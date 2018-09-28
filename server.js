2ecbb762cae03de414a699ea140aa961eeb74cb3
Shampu <cbuchhauser@gmail.com>
2018-09-27 18:44:26 -0700

    Fixed note box

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ var cheerio = require("cheerio");
 // Require all models
 var db = require("./models");
 
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 
 // Initialize Express
 var app = express();
@@ -30,6 +30,8 @@ app.use(express.static("public"));
 mongoose.connect("mongodb://localhost/newsscraperdb");
 
 // Routes
+
+
 
 // A GET route for scraping the echoJS website
 app.get("/scrape", function(req, res) {
